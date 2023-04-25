a1da25c302132dcb14cd6ab88991746d0422d112
rimamenaa <rimamenaa98@gmail.com>
2023-04-26 00:55:46 +0100

    responsiveness in progress

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -6,6 +6,11 @@ export default function Welcome(props) {
   return (
     <>
     <div className="welcome">
+      <div className='welcomeText'>
+      <h1>Welcome peeps!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iure eum explicabo ab omnis. Eum quidem ipsam voluptatem dolores, eos animi atque id explicabo cumque delectus vero laudantium. Ad, illum!</p>
+      </div>
+      
       <img src={props.welcomeImg} className='welcomeImg' alt="" />
     </div>  
     </>
