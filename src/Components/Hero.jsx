c3f127d66e4c068fb49ac47283d3e29f7bea9f6a
rimamenaa <rimamenaa98@gmail.com>
2023-04-25 23:30:46 +0100

    fixed navbar bug

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import "./HeroStyles.css"
-import Image from '../assets/1.jpg'
 
 export default function Hero(props) {
   return (
     <>
-    <div className='hero'>
-        <img src={Image} alt="HeroImg" />
+    <div className={props.cName}>
+        <img src={props.heroImg} alt="HeroImg" />
         <div className='heroText'>
-            <h1>Dar Kenza</h1>
-            <p>GUEST HOUSE</p>
+            <h1>{props.title}</h1>
+            <p>{props.text}</p>
         </div>
     </div>
     
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
+import Image from '../assets/12.jpg'
 
 export default function Home() {
   return (
     <>
     <Navbar/>
-    <Hero/>
+    <Hero
+    cName="hero"
+    heroImg={Image}
+    title="Dar Kenza"
+    text="GUEST HOUSE"
+    />
     </>
   )
 }
