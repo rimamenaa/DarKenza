b31a94395bab87193ec8fcae8e9fb67eaa34a972
rimamenaa <rimamenaa98@gmail.com>
2023-04-26 23:13:34 +0100

    style

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -10,7 +10,7 @@ export default function Welcome(props) {
       <div className="first-des">
         <div className="des-text">
           <h1>Your Ultimate Beach Retreat, Where Serenity and Vintage Charm Meet</h1>
-            <p>Welcome to Dar Kenza, where you can escape the chaos of city life and indulge in a serene, picturesque setting overlooking Kelibia Beach. <br/> Our spacious and unique rooms are designed with traditional vintage decor that exudes charm and character, providing you with a truly authentic experience. Immerse yourself in the peaceful atmosphere and let our exceptional hospitality take care of the rest. <br/> Whether you're looking for a romantic getaway or a family vacation, we're here to create unforgettable memories for you and your loved ones. Book your stay today and discover the perfect blend of tradition and nature at Dar Kenza.</p>
+          <p>Welcome to Dar Kenza, where you can escape the chaos of city life and indulge in a serene, picturesque setting overlooking Kelibia Beach. <br/> Our spacious and unique rooms are designed with traditional vintage decor that exudes charm and character, providing you with a truly authentic experience. Immerse yourself in the peaceful atmosphere and let our exceptional hospitality take care of the rest. <br/> Whether you're looking for a romantic getaway or a family vacation, we're here to create unforgettable memories for you and your loved ones. Book your stay today and discover the perfect blend of tradition and nature at Dar Kenza.</p>
         </div>
         
       <div className='welcomeImg'>
